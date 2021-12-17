3ea0586c4a577c4a3f3a23051946eb1899f16678
Gavin Matthews <gmatthews@axway.com>
2021-12-17 12:09:11 +0000

    POC-0014 test resources.

--- clean.sh
+++ clean.sh
@@ -2,7 +2,8 @@
 
 axway auth login
 
-axway central delete asset/musicalinstruments
-axway central delete stage/demo
-axway central delete -f 1-env-staging.yaml
-axway central delete -f 2-category-demo.yaml
+axway central delete asset musicalinstruments -y
+axway central delete stage demo -y
+axway central delete environment staging -y
+axway central delete category demo -y
+axway central delete integration assetprovisioning -y
--- run.sh
+++ run.sh
@@ -2,18 +2,19 @@
 
 axway auth login
 
-axway central apply -f 1-env-staging.yaml
-axway central apply -f 2-category-demo.yaml
-axway central apply -f 3-stage-demo.yaml
-axway central apply -f 4-accessrequestdefinition-demo.yaml
-axway central apply -f 5-assetmappingtemplate-staging.yaml
+axway central apply -f 01-env-staging.yaml
+axway central apply -f 02-category-demo.yaml
+axway central apply -f 03-stage-demo.yaml
+axway central apply -f 04-accessrequestdefinition-demo.yaml
+axway central apply -f 05-resourcehook.yaml
+axway central apply -f 06-assetmappingtemplate-staging.yaml
 
-axway central apply -f 6-apiservice-music.yaml
-axway central apply -f 7-assetmapping-music.yaml
+axway central apply -f 07-apiservice-music.yaml
+axway central apply -f 08-assetmapping-music.yaml
 
 # Twiddle our thumbs waiting for asset creation
 sleep 20
-axway central apply -f 8-releasetag-music.yaml
+axway central apply -f 09-releasetag-music.yaml
 
 
 axway central get environment,stage,category,apiservice,asset,assetrelease
